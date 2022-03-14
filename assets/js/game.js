bb7af7ee29d125ab9a89596fa39084d263f31097
Chrisborer <chris.borer@gmail.com>
2022-03-13 19:56:17 -0500

    version.1.0.1 robot's name

---
+++ assets/js/game.js
@@ -0,0 +1,13 @@
+//window.alert("This is an alert! JavaScript is running!");
+//Game Functions
+function fight() {
+    window.alert("The fight has begun");
+}
+var playerName = window.prompt("What is the robot's name?");
+console.log(playerName);
+console.log("This logs a string, good for leaving yourself a message");
+console.log(10 + 10);
+console.log("Our robots's name is " + playerName);
+
+// function Calls
+// fight();
