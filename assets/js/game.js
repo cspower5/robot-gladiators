4df982626bc024e9b9416c3fb4ccbf073a14ed88
Chrisborer <chris.borer@gmail.com>
2022-03-13 22:57:31 -0500

    version.1.0.2 added for loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,13 +2,13 @@ var playerName = window.prompt("What is your robot's nane?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
+//console.log(playerName, playerAttack, playerHealth);
 
-console.log(playerName, playerAttack, playerHealth);
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+var fight = function(enemyName) {
     // Alert players that they are starting the round
     window.alert("Welcome to robot Gladiators!");
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or  'SKIP to choose.");
@@ -52,4 +52,6 @@ var fight = function() {
         }
     };
 //Function calls
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
